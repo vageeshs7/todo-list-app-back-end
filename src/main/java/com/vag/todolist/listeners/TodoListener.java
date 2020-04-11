64d1be87f092866502a54e5c4c3f2bd44233f253
vageesh <vageeshs7@gmail.com>
2020-04-11 19:55:01 +0530

    JMS listener fix2

--- src/main/java/com/vag/todolist/listeners/TodoListener.java
+++ src/main/java/com/vag/todolist/listeners/TodoListener.java
@@ -4,12 +4,11 @@ import com.vag.todolist.config.MessagingConfig;
 import com.vag.todolist.dom.Todo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
 
+@Component
 public class TodoListener {
-
-    @Autowired
-    private MessagingConfig messagingConfig;
-
+    
     @JmsListener(destination = "todo-save-queue", containerFactory = "todoMessagingFactory")
     public void receiveMessage(Todo todo) {
         System.out.println("Received <" + todo + ">");
