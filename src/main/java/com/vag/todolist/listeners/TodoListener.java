d7f361bc8804027c987eff0875835ffac8396bc4
vageesh <vageeshs7@gmail.com>
2020-04-12 19:01:56 +0530

    update jms listener argument

--- src/main/java/com/vag/todolist/listeners/TodoListener.java
+++ src/main/java/com/vag/todolist/listeners/TodoListener.java
@@ -2,15 +2,24 @@ package com.vag.todolist.listeners;
 
 import com.vag.todolist.config.MessagingConfig;
 import com.vag.todolist.dom.Todo;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
+
 @Component
 public class TodoListener {
-    
+    private Logger logger = LogManager.getLogger(MessagingConfig.class);
     @JmsListener(destination = "todo-save-queue", containerFactory = "todoMessagingFactory")
-    public void receiveMessage(Todo todo) {
-        System.out.println("Received <" + todo + ">");
+    public void receiveMessage(Message message) {
+        try {
+            logger.info("Received <" + message.getJMSMessageID() + ">" + message.getClass());
+        } catch (JMSException e) {
+            logger.error(e);
+        }
     }
 }
