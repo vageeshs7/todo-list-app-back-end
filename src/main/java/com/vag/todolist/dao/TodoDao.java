86440ff2c3a1967b499988e9b046ba1be57f52c3
vageesh <vageeshs7@gmail.com>
2020-05-25 18:51:49 +0530

    Update DAO

--- src/main/java/com/vag/todolist/dao/TodoDao.java
+++ src/main/java/com/vag/todolist/dao/TodoDao.java
@@ -3,6 +3,6 @@ package com.vag.todolist.dao;
 import com.vag.todolist.dom.Todo;
 import org.springframework.data.repository.CrudRepository;
 
-public interface TodoDao extends CrudRepository<Todo, String> {
+public interface TodoDao extends CrudRepository<Todo, Integer> {
     public Todo findByUser(String user);
 }
